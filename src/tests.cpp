1b36c24f2e944dfc097c5bb541f6e7bebafe0de7
GaryNLOL <garypastel@gmail.com>
2021-04-28 16:25:28 -0500

    Corrected tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,3 +1,4 @@
+#define GRAPHITE_LOGGER__USE_DEFAULT_LOG_LEVELS
 #include "../include/Graphite-Logger/logging.hpp"
 
 int main()
@@ -10,4 +11,14 @@ int main()
     logger[my_own_level_1] << "Some important data that will be shown.";
     logger[my_own_level_2] << "Some not as important data that will be shown.";
     logger[my_own_level_3] << "Some data that will not be shown.";
+
+    using namespace GraphiteLogger::LogLevels;
+    logger = GraphiteLogger::Logger("UsefulLogger",critical);
+    logger[emergency] << "Terminating program... Exit code: " << 4;
+    logger[critical] << "Undefined behaviour detected.";
+    logger[error] << "atoi() cannot transform \"479878754524347787864456546\" (out of range).";
+    logger[warning] << "Comparison of integers of different flavors.";
+    logger[notice] << "Pass by const reference would be more efficient than past by value";
+    logger[info] << "Exited struggleFunction() successfully.";
+    logger[debug] << "i is " << 1;
 }
