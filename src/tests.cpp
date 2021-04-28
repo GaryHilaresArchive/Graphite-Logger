fa134cbfdc8ac384e1ab710d3cb0ae3922f219cd
GaryNLOL <garypastel@gmail.com>
2021-04-28 16:19:46 -0500

    Modified tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -6,11 +6,11 @@ int main()
     using namespace GraphiteLogger::LogLevels;
     GraphiteLogger::Logger logger("UsefulLogger",debug);
     logger.addOutput(&std::cout);
-    logger[emergency] << "Terminating program...";
+    logger[emergency] << "Terminating program... Exit code: " << 4;
     logger[critical] << "Undefined behaviour detected.";
     logger[error] << "atoi() cannot transform \"479878754524347787864456546\" (out of range).";
     logger[warning] << "Comparison of integers of different flavors.";
     logger[notice] << "Pass by const reference would be more efficient than past by value";
     logger[info] << "Exited struggleFunction() successfully.";
-    logger[debug] << "i is 1";
+    logger[debug] << "i is " << 1;
 }
