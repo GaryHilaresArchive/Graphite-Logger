55e0f6dbdaff3f24b85ca99aaf17600c33992d7e
GaryNLOL <garypastel@gmail.com>
2021-04-28 15:11:21 -0500

    Added log levels

--- include/Graphite-Logger/logging.hpp
+++ include/Graphite-Logger/logging.hpp
@@ -8,74 +8,82 @@
 
 namespace GraphiteLogger
 {
-    namespace Logging
+    #ifdef GRAPHITE_LOGGER__USE_DEFAULT_LOG_LEVELS
+    namespace LogLevels
     {
-        using GraphiteLogger::__hidden__::convertToString;
-        class Logger
+        constexpr int emergency = 0;
+        constexpr int alert = 1;
+        constexpr int critical = 2;
+        constexpr int error = 3;
+        constexpr int warning = 4;
+        constexpr int notice = 5;
+        constexpr int info = 6;
+        constexpr int debug = 7;
+    }
+    #endif
+    using GraphiteLogger::__hidden__::convertToString;
+    class Logger
+    {
+    private:
+        std::string name;
+        int log_level;
+        std::vector<std::ostream*> outputs;
+        template<typename First>
+        static std::string getMessage(First first)
+        {
+            return convertToString(first);
+        }
+        template<typename First, typename... Args>
+        static std::string getMessage(First first, Args... args)
+        {
+            return convertToString(first) + getMessage(args...);
+        }
+        class LoggerHelper
         {
         private:
-            std::string name;
-            std::vector<std::ostream*> outputs;
-            template<typename First>
-            std::string getMessage(First first)
+            std::vector<std::ostream*>& outputs;
+            bool do_ignore;
+            template<typename T>
+            std::string getMessage(T element)
             {
-                return convertToString(first);
+                return convertToString(element);
             }
-            template<typename First, typename... Args>
-            std::string getMessage(First first, Args... args)
-            {
-                return convertToString(first) + getMessage(args...);
-            }
-            class LoggerHelper
-            {
-            private:
-                template<typename First>
-                std::string getMessage(First first)
-                {
-                    return convertToString(first);
-                }
-                template<typename First, typename... Args>
-                std::string getMessage(First first, Args... args)
-                {
-                    return convertToString(first) + getMessage(args...);
-                }
-                std::vector<std::ostream*>& outputs;
-            public:
-                LoggerHelper(std::vector<std::ostream*>& _outputs): outputs(_outputs) {}
-                template<typename... Args>
-                LoggerHelper& operator<<(Args... args)
-                {
-                    std::string arrangedMessage = getMessage(args...);
-                    for(unsigned int i = 0; i < this->outputs.size(); i++)
-                        *(this->outputs[i]) << arrangedMessage;
-                    return *this;
-                }
-                ~LoggerHelper()
-                {
-                    for(unsigned int i = 0; i < this->outputs.size(); i++)
-                        *this->outputs[i] << ".\n";
-                }
-            };
         public:
-            Logger(const std::vector<std::ostream*>& outputs = {}, const std::string& name = "Logger")
+            LoggerHelper(std::vector<std::ostream*>& new_outputs, bool new_do_ignore = false): outputs(new_outputs), do_ignore(new_do_ignore) {}
+            template<typename T>
+            LoggerHelper& operator<<(T element)
             {
-                this->name = name;
-                this->outputs = outputs;
-            }
-            template<typename... Args>
-            LoggerHelper operator<<(Args... args)
-            {
-                std::time_t now = std::time(0);
-                std::string nowInStr = std::ctime(&now);
-                std::string arrangedMessage = (std::string) "[" + nowInStr.substr(0,nowInStr.size()-1) + "] " + name + ": " + getMessage(args...);
+                if(this->do_ignore)
+                    return *this;
+                std::string arrangedMessage = getMessage(element);
                 for(unsigned int i = 0; i < this->outputs.size(); i++)
                     *(this->outputs[i]) << arrangedMessage;
-                return LoggerHelper(this->outputs);
+                return *this;
             }
-            void addOutput(std::ostream* stream)
+            ~LoggerHelper()
             {
-                this->outputs.push_back(stream);
+                if(this->do_ignore)
+                    return;
+                for(unsigned int i = 0; i < this->outputs.size(); i++)
+                    *this->outputs[i] << "\n";
             }
         };
-    }
+    public:
+        Logger(const std::string& new_name, int new_log_level, const std::vector<std::ostream*>& new_outputs = {}): name(new_name), log_level(new_log_level), outputs(new_outputs) {}
+        void addOutput(std::ostream* stream)
+        {
+            this->outputs.push_back(stream);
+        }
+        LoggerHelper operator[](int message_log_level)
+        {
+            if(this->log_level < message_log_level)
+                return LoggerHelper(this->outputs,true);
+            std::time_t now = std::time(0);
+            std::string nowInStr = std::ctime(&now);
+            std::string arrangedMessage = (std::string) "[" + nowInStr.substr(0,nowInStr.size()-1) + "] " + name + ": ";
+            for(unsigned int i = 0; i < this->outputs.size(); i++)
+                *(this->outputs[i]) << arrangedMessage;
+            return LoggerHelper(this->outputs);
+        }
+    };
 }
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,8 +1,16 @@
+#define GRAPHITE_LOGGER__USE_DEFAULT_LOG_LEVELS
 #include "../include/Graphite-Logger/logging.hpp"
 
 int main()
 {
-    GraphiteLogger::Logging::Logger logger;
+    using namespace GraphiteLogger::LogLevels;
+    GraphiteLogger::Logger logger("UsefulLogger",debug);
     logger.addOutput(&std::cout);
-    logger << "Test";
+    logger[emergency] << "Terminating program...";
+    logger[critical] << "Undefined behaviour detected.";
+    logger[error] << "atoi() cannot transform \"479878754524347787864456546\" (out of range).";
+    logger[warning] << "Comparison of integers of different flavors.";
+    logger[notice] << "Pass by const reference would be more efficient than past by value";
+    logger[info] << "Exited struggleFunction() successfully.";
+    logger[debug] << "i is 1";
 }
