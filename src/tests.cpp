0c4c7191e5a9438e97b266f67d179980cb68851a
GaryNLOL <garypastel@gmail.com>
2021-04-28 19:40:14 -0500

    Added comment to using namespace instructions

--- src/tests.cpp
+++ src/tests.cpp
@@ -12,7 +12,7 @@ int main()
     logger[my_own_level_2] << "Some not as important data that will be shown.";
     logger[my_own_level_3] << "Some data that will not be shown.";
 
-    using namespace GraphiteLogger::LogLevels;
+    using namespace GraphiteLogger::LogLevels; // Namespace of "emergency", "critical", "error", etc.
     logger.setLogLevel(critical);
     logger[emergency] << "Terminating program... Exit code: " << 4;
     logger[critical] << "Undefined behaviour detected.";
